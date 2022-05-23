81e20b301f87e220151aa449fdb7186f67327ade
Cesar <crafael@pucp.edu.pe>
2022-05-23 14:28:27 -0500

    Initial Commit 2

---
+++ src/main/java/com/pucp/odiparpackappback/OdiparPackAppBackApplication.java
@@ -0,0 +1,13 @@
+package com.pucp.odiparpackappback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdiparPackAppBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdiparPackAppBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pucp/odiparpackappback/OdiparPackAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pucp.odiparpackappback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdiparPackAppBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
