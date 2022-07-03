3908a7f50c18e8c771a8592f167a51e79b1e60db
Cesar <crafael@pucp.edu.pe>
2022-07-03 00:14:27 -0500

    cambios

--- src/main/java/com/pucp/odiparpackappback/Repositories/BloqueoRepository.java
+++ src/main/java/com/pucp/odiparpackappback/Repositories/BloqueoRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 
@@ -14,6 +13,6 @@ public interface BloqueoRepository extends CrudRepository<BloqueoModel, Long> {
     @Query("select b from BloqueoModel b where ((b.ubigeoInicio = ?1 and b.ubigeoFin = ?2) or (b.ubigeoInicio = ?2 and b.ubigeoFin = ?1)) and b.fechaInicio <= ?3 and ?4 <= b.fechaFin")
     List<BloqueoModel> findBloqueoModelByUbigeoInicioAndUbigeoFin(int ubigeoInicio, int ubigeoFin, Date fechaInicio, Date fechaFin);
 
-    @Query("select b from BloqueoModel b where b.fechaInicio >= ?1 and ?2 >= b.fechaFin")
+    @Query("select b from BloqueoModel b where b.fechaInicio <= ?1 and ?2 <= b.fechaFin")
     List<BloqueoModel> findBloqueoModelByFechaInicioAndAndFechaFin(Date fechaInicio, Date fechaFin);
 }
