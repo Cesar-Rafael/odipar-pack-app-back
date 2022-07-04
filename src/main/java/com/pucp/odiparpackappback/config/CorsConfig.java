79cb4b0cc41fa86d1d375da42a84d64028aa5e77
GianPRodriguez <gianp.rodriguezp@pucp.pe>
2022-07-03 19:14:38 -0500

    push

--- src/main/java/com/pucp/odiparpackappback/config/CorsConfig.java
+++ src/main/java/com/pucp/odiparpackappback/config/CorsConfig.java
@@ -3,7 +3,9 @@ package com.pucp.odiparpackappback.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
@@ -18,3 +20,4 @@ public class CorsConfig implements WebMvcConfigurer {
         };
     }
 }
+
---
+++ src/main/java/com/pucp/odiparpackappback/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.pucp.odiparpackappback.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebSecurity
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
