84abf22929bf22c038bcb291a490e275228a48cf
Cesar <crafael@pucp.edu.pe>
2022-07-05 19:09:02 -0500

    ga

--- src/main/java/com/pucp/odiparpackappback/controllers/PedidoController.java
+++ src/main/java/com/pucp/odiparpackappback/controllers/PedidoController.java
@@ -5,8 +5,6 @@ import com.pucp.odiparpackappback.Repositories.PedidoRepository;
 import com.pucp.odiparpackappback.dto.Simulation;
 import com.pucp.odiparpackappback.models.*;
 import com.pucp.odiparpackappback.services.algorithm.ABC;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -14,7 +12,7 @@ import java.time.ZoneId;
 import java.util.*;
 
 @RestController
-@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
 public class PedidoController {
     private final PedidoRepository pedidoRepository;
 
@@ -23,25 +21,25 @@ public class PedidoController {
     }
 
     @GetMapping("/Pedido/Listar")
-    @CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
+    @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
     public List<PedidoModel> listarPedidos() {
         return (List<PedidoModel>) pedidoRepository.findAll();
     }
 
     @GetMapping("/Pedido/{id}")
-    @CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
+    @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
     Optional<PedidoModel> ListarPedidoxId(@PathVariable("id") long id) {
         return pedidoRepository.findById(id);
     }
 
     @PostMapping("/Pedido/Insertar")
-    @CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
+    @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
     PedidoModel InsertarPedido(@RequestBody PedidoModel pedidoModel) {
         return pedidoRepository.save(pedidoModel);
     }
 
     @PostMapping("/Pedido/Insertar/Masivo")
-    @CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
+    @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
     boolean InsertarListaPedidos(@RequestBody List<PedidoModel> pedidosModel) {
         try {
             pedidoRepository.saveAll(pedidosModel);
@@ -52,7 +50,7 @@ public class PedidoController {
     }
 
     @GetMapping("/ABCDD")
-    @CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
+    @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
     public boolean ejecutarABCDiaDia() {
         ABC abc = new ABC();
 
@@ -85,12 +83,12 @@ public class PedidoController {
     }
 
     @GetMapping("/SimulacionReporte")
-    @CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
+    @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
     Map<String, Double> SimulacionReporte() {
         // REPORTE EXTERNO
         HashMap<Long, Long> pedidos = new HashMap<>();
         HashMap<Integer, Integer> oficinas = new HashMap<>();
-        for(int i = 0; i < Mapa.oficinas.size(); i++){
+        for (int i = 0; i < Mapa.oficinas.size(); i++) {
             oficinas.put(Mapa.oficinas.get(i).getUbigeo(), Mapa.oficinas.get(i).getRegion().getCode());
         }
         Double numRutas = Mapa.rutasSimulacion.size() + 0.0;
@@ -112,8 +110,8 @@ public class PedidoController {
             }
         }
         for (int i = 0; i < Mapa.pedidosSimulacion.size(); i++) {
-            if(oficinas.get(Mapa.pedidosSimulacion.get(i).getIdCiudadDestino())  == 0) numCosta++;
-            else if(oficinas.get(Mapa.pedidosSimulacion.get(i).getIdCiudadDestino())  == 1) numSierra++;
+            if (oficinas.get(Mapa.pedidosSimulacion.get(i).getIdCiudadDestino()) == 0) numCosta++;
+            else if (oficinas.get(Mapa.pedidosSimulacion.get(i).getIdCiudadDestino()) == 1) numSierra++;
             else numSelva++;
             if (pedidos.containsKey(Mapa.pedidosSimulacion.get(i).getId())) {
                 tiempo += pedidos.get(Mapa.pedidosSimulacion.get(i).getId()) - Mapa.pedidosSimulacion.get(i).getFechaHoraCreacion().getTime() / 1000;
@@ -127,11 +125,10 @@ public class PedidoController {
         map.put("cantCosta", numCosta + 0.0);
         map.put("cantSierra", numSierra + 0.0);
         map.put("cantSelva", numSelva + 0.0);
-        map.put("pedidosParcialesxPedido", pedidosParcialesxPedido/numPedidos);
-        if(Mapa.flagColapso){
+        map.put("pedidosParcialesxPedido", pedidosParcialesxPedido / numPedidos);
+        if (Mapa.flagColapso) {
             map.put("colasoLogistico", 1.0);
-        }
-        else{
+        } else {
             map.put("colasoLogistico", 0.0);
         }
 
@@ -140,7 +137,7 @@ public class PedidoController {
 
 
     @PostMapping("/ABCS")
-    @CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
+    @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
     boolean ejecutarABCSimulacion(@RequestBody Simulation simulation) {
         ABC abc = new ABC();
 
@@ -172,16 +169,11 @@ public class PedidoController {
                 System.out.println();
             }
         }
-        if(Mapa.flagColapso){
-            return false;
-        }
-        else{
-            return true;
-        }
+        return Mapa.flagColapso;
     }
 
     @GetMapping("/simulacion/reiniciar")
-    @CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
+    @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
     boolean pararSimulacion() {
         Mapa.pedidosSimulacion.clear();
         Mapa.rutasSimulacion.clear();
@@ -190,7 +182,7 @@ public class PedidoController {
     }
 
     @PostMapping("/PararDiaDia")
-    @CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
+    @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
     boolean pararDiaDia() {
         Mapa.setFlag(false);
         Mapa.pedidosSimulacion.clear();
@@ -201,13 +193,13 @@ public class PedidoController {
 
     @GetMapping("/Pedido/ListarRutasSimulacionxIdPedido/{idPedido}")
     @ResponseBody
-    @CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
+    @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
     public List<RutaConArraySegHorasLl> ListarRutasSimulacionxIdPedido(@PathVariable("idPedido") long idPedido) {
         try {
             List<RutaConArraySegHorasLl> auxRutasG = new ArrayList<>();
-            for(int i=0; i<Mapa.rutasSimulacion.size(); i++){
-                for(int j=0; j<Mapa.rutasSimulacion.get(i).getPedidosParciales().size(); j++){
-                    if(Mapa.rutasSimulacion.get(i).getPedidosParciales().get(j).getIdPedido() == idPedido){
+            for (int i = 0; i < Mapa.rutasSimulacion.size(); i++) {
+                for (int j = 0; j < Mapa.rutasSimulacion.get(i).getPedidosParciales().size(); j++) {
+                    if (Mapa.rutasSimulacion.get(i).getPedidosParciales().get(j).getIdPedido() == idPedido) {
                         ArrayList<Integer> auxAI = new ObjectMapper().reader(List.class).readValue(Mapa.rutasSimulacion.get(i).getSeguimiento());
                         ArrayList<String> auxNombreProvincias = new ArrayList<>();
                         for (int zz = 0; zz < auxAI.size(); zz++) {
@@ -226,18 +218,18 @@ public class PedidoController {
                         ArrayList<PedidoParcialModel> pedidosParciales = new ArrayList<>();
                         ArrayList<PedidoModel> pedidos = new ArrayList<>();
                         for (int c = 0; c < Mapa.pedidosSimulacion.size(); c++) {
-                            if(Mapa.pedidosSimulacion.get(c).getId() == idPedido){
+                            if (Mapa.pedidosSimulacion.get(c).getId() == idPedido) {
                                 //
-                                for(int zzz=0; zzz<Mapa.oficinas.size();zzz++){
-                                    if(Mapa.oficinas.get(zzz).getUbigeo() == Mapa.pedidosSimulacion.get(c).getIdCiudadDestino()){
+                                for (int zzz = 0; zzz < Mapa.oficinas.size(); zzz++) {
+                                    if (Mapa.oficinas.get(zzz).getUbigeo() == Mapa.pedidosSimulacion.get(c).getIdCiudadDestino()) {
                                         Mapa.pedidosSimulacion.get(c).setCiudadDestino(Mapa.oficinas.get(zzz).getProvincia());
                                     }
                                 }
                                 //
                                 pedidos.add(Mapa.pedidosSimulacion.get(c));
-                                for(int contador = 0; contador < Mapa.rutasSimulacion.size(); contador++){
-                                    for(int contador2 = 0; contador2 < Mapa.rutasSimulacion.get(contador).getPedidosParciales().size(); contador2++){
-                                        if(Mapa.rutasSimulacion.get(contador).getPedidosParciales().get(contador2).getIdPedido() == idPedido){
+                                for (int contador = 0; contador < Mapa.rutasSimulacion.size(); contador++) {
+                                    for (int contador2 = 0; contador2 < Mapa.rutasSimulacion.get(contador).getPedidosParciales().size(); contador2++) {
+                                        if (Mapa.rutasSimulacion.get(contador).getPedidosParciales().get(contador2).getIdPedido() == idPedido) {
                                             pedidosParciales.add(Mapa.rutasSimulacion.get(contador).getPedidosParciales().get(contador2));
                                         }
                                     }
@@ -258,13 +250,13 @@ public class PedidoController {
 
     @GetMapping("/Pedido/ListarRutasDiaDiaxIdPedido/{idPedido}")
     @ResponseBody
-    @CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
+    @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
     public List<RutaConArraySegHorasLl> ListarRutasDiaDiaxIdPedido(@PathVariable("idPedido") long idPedido) {
         try {
             List<RutaConArraySegHorasLl> auxRutasG = new ArrayList<>();
-            for(int i=0; i<Mapa.rutasDiaDia.size(); i++){
-                for(int j=0; j<Mapa.rutasDiaDia.get(i).getPedidosParciales().size(); j++){
-                    if(Mapa.rutasDiaDia.get(i).getPedidosParciales().get(j).getIdPedido() == idPedido){
+            for (int i = 0; i < Mapa.rutasDiaDia.size(); i++) {
+                for (int j = 0; j < Mapa.rutasDiaDia.get(i).getPedidosParciales().size(); j++) {
+                    if (Mapa.rutasDiaDia.get(i).getPedidosParciales().get(j).getIdPedido() == idPedido) {
                         ArrayList<Integer> auxAI = new ObjectMapper().reader(List.class).readValue(Mapa.rutasDiaDia.get(i).getSeguimiento());
                         ArrayList<String> auxNombreProvincias = new ArrayList<>();
                         for (int zz = 0; zz < auxAI.size(); zz++) {
@@ -283,18 +275,18 @@ public class PedidoController {
                         ArrayList<PedidoParcialModel> pedidosParciales = new ArrayList<>();
                         ArrayList<PedidoModel> pedidos = new ArrayList<>();
                         for (int c = 0; c < Mapa.pedidosDiaDia.size(); c++) {
-                            if(Mapa.pedidosDiaDia.get(c).getId() == idPedido){
+                            if (Mapa.pedidosDiaDia.get(c).getId() == idPedido) {
                                 //
                                 pedidos.add(Mapa.pedidosDiaDia.get(c));
-                                for(int zzz=0; zzz<Mapa.oficinas.size();zzz++){
-                                    if(Mapa.oficinas.get(zzz).getUbigeo() == Mapa.pedidosDiaDia.get(c).getIdCiudadDestino()){
+                                for (int zzz = 0; zzz < Mapa.oficinas.size(); zzz++) {
+                                    if (Mapa.oficinas.get(zzz).getUbigeo() == Mapa.pedidosDiaDia.get(c).getIdCiudadDestino()) {
                                         Mapa.pedidosDiaDia.get(c).setCiudadDestino(Mapa.oficinas.get(zzz).getProvincia());
                                     }
                                 }
                                 //
-                                for(int contador = 0; contador < Mapa.rutasDiaDia.size(); contador++){
-                                    for(int contador2 = 0; contador2 < Mapa.rutasDiaDia.get(contador).getPedidosParciales().size(); contador2++){
-                                        if(Mapa.rutasDiaDia.get(contador).getPedidosParciales().get(contador2).getIdPedido() == idPedido){
+                                for (int contador = 0; contador < Mapa.rutasDiaDia.size(); contador++) {
+                                    for (int contador2 = 0; contador2 < Mapa.rutasDiaDia.get(contador).getPedidosParciales().size(); contador2++) {
+                                        if (Mapa.rutasDiaDia.get(contador).getPedidosParciales().get(contador2).getIdPedido() == idPedido) {
                                             pedidosParciales.add(Mapa.rutasDiaDia.get(contador).getPedidosParciales().get(contador2));
                                         }
                                     }
