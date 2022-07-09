5d329dd20e1199ce73e86c3782537be3d6393d5b
GianPRodriguez <gianp.rodriguezp@pucp.pe>
2022-07-09 17:58:19 -0500

    push

--- src/main/java/com/pucp/odiparpackappback/services/algorithm/ABC.java
+++ src/main/java/com/pucp/odiparpackappback/services/algorithm/ABC.java
@@ -117,8 +117,8 @@ public class ABC {
         for (int a = 0; a < rutas.size(); a++) {
             // Se verifica si termino
             if (opcion == 0 && rutas.get(a).getHorasDeLlegada().get(rutas.get(a).getHorasDeLlegada().size() - 1) > fin.atZone(zoneId).toEpochSecond()) {
-                rutas.get(a).setFlagTerminado(true);
-                vehiculos.get(Math.toIntExact(rutas.get(a).getIdUnidadTransporte())).setEstado(EstadoUnidadTransporte.DISPONIBLE);
+                rutas.get(a).setFlagTerminado(false);
+                //vehiculos.get(Math.toIntExact(rutas.get(a).getIdUnidadTransporte())).setEstado(EstadoUnidadTransporte.DISPONIBLE);
             }
             // Se verifica si el pedido puede ser asignado a esa ruta
             asignado = asignarPedidoRutaVehiculo(pedido, rutas.get(a), opcion);
